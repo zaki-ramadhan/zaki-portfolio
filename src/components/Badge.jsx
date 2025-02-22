51e895125fc0670182c7ed63c372c04cd749f692
zaki-ramadhan <zakiram4dhan@gmail.com>
2025-02-22 11:40:34 +0700

    layout adjustment

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -4,13 +4,13 @@ import headAvatar from '../assets/images/me_ai_vers_2(big_head).png'
 
 const Badge = () => {
   return (
-    <span className='badge w-fit p-2 rounded-lg flex gap-4'>
-      <div className="avatar w-10 h-10 rounded-full bg-additional">
+    <span className='badge w-fit p-2 rounded-lg flex items-center gap-4'>
+      <div className="avatar w-12 h-12 lg:w-10 lg:h-10 rounded-full bg-additional">
           <img src={headAvatar} alt='headAvatar' className='scale-120'/>
       </div>
         <div className='bio__wrp'>
-            <h2>Hi, I&apos;m Zaki.</h2>
-            <p className='text-secondary text-xs'>Front-end developer</p>
+            <h2 className=' text-2xl lg:text-base'>Hi, I&apos;m Zaki.</h2>
+            <p className='text-secondary text-lg lg:text-xs'>Front-end developer</p>
         </div>
     </span>
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,11 +5,11 @@ const Header = () => {
 	return (
 		<section className="container mx-auto px-6 md:px-20 pt-7 pb-4 flex justify-between">
 			<div className="dev-name">
-                <h1 className="inline text-2xl lg:text-base">Zaki Ramadhan</h1>
-                <span className="text-secondary">, Front-End</span>
+                <h1 className="inline text-xl lg:text-base">Zaki R<span className="hidden lg:inline-block">amadhan</span></h1>
+                <span className="text-secondary text-xl lg:text-base">, Front-End</span>
             </div>
 			<nav>
-                <ul className="hidden md:flex gap-12 text-secondary">
+                <ul className="hidden md:flex gap-12 text-secondary text-lg lg:text-base">
                     <li><a href="" className="hover:text-white duration-100">Present</a></li>
                     <li><a href="#projects" className="hover:text-white duration-100">Projects</a></li>
                     <li><a href="" className="hover:text-white duration-100">Contact</a></li>
