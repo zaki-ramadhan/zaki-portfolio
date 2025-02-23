5979d17100e17a1041d17144367b9618f06d599e
zaki-ramadhan <zakiram4dhan@gmail.com>
2025-02-24 06:07:37 +0700

    shortened the code structure in the cards file and used data from the ProjectCards file, adjusted the TechIcon size

---
+++ src/components/ProjectCards.jsx
@@ -0,0 +1,79 @@
+/* eslint-disable react/prop-types */
+import { Icon } from "@iconify-icon/react";
+import TechIcons from "./TechIcon";
+import zappifyPreview from "../assets/images/ss_pricing_page.png";
+import katIbuPreview from "../assets/images/ss_dashboard_admin_katering_ibu.png";
+
+const projectData = [
+    {
+        id: 1,
+        name: "Zappify",
+        desc: "Simplify Investing, Maximize Returns",
+        link: "https://github.com/zaki-ramadhan/zappify?tab=readme-ov-file#zappify",
+        status: "In Progress",
+        preview: zappifyPreview,
+        colors: {
+            titleColor: 'text-white/80',
+            bgColor: 'from-emerald-800 to-zappify-darker',
+            btnColor: 'bg-secondary/20',
+            iconsBgColor: 'bg-white/5',
+            overlayColor: 'from-zappify-darker to-zappify-darker/0',
+        }
+    },
+    {
+        id: 2,
+        name: "Katering Ibu",
+        desc: "Admin Dashboard for Katering Ibu",
+        link: "https://github.com/zaki-ramadhan/katering-ibu?tab=readme-ov-file#katering-ibu-a-laravel-based-catering-website",
+        status: "Maintenance",
+        preview: katIbuPreview,
+        colors: {
+            titleColor: 'text-primary',
+            bgColor: 'from-white to-slate-200',
+            btnColor: 'bg-secondary/10',
+            iconsBgColor: 'bg-white/20',
+            overlayColor: 'from-slate-200 to-slate-200/0',
+        }
+    }
+];
+
+const ProjectCards = ({ is_image, className }) => {
+    return (
+        <>
+            {projectData.map((project) => {
+                const { titleColor, bgColor, btnColor, overlayColor } = project.colors;
+
+                return (
+                    <div key={project.id} className={`project-card relative h-90 lg:h-110 bg-radial-[at_100%_-40%] ${bgColor} to-60% ${!is_image ? 'p-7 sm:p-10' : ''} ${className} rounded-[2.7rem] flex flex-col border border-additional/10 overflow-hidden`}>
+                        <div>
+                            <div className="container title-status__wrp flex items-center justify-between">
+                                <span className="title-icon__wrp flex items-center gap-2 basis-1/2">
+                                    <h1 className={`${titleColor} text-2xl md:text-3xl font-Archivo max-w-[8ch] md:max-w-none overflow-hidden text-ellipsis whitespace-nowrap`}>
+                                        {project.name}
+                                    </h1>
+                                    <a href={project.link} target="_blank" rel="noopener noreferrer">
+                                        <Icon icon="stash:arrow-up-light" width="24" height="24" className={`${titleColor} rotate-45 scale-95 md:scale-100 -mt-1 md:-mt-0 md:ml-0 p-1.5 ${btnColor} hover:bg-secondary/30 active:bg-secondary/30 rounded-full ms-1 translate-y-1 cursor-pointer`} />
+                                    </a>
+                                </span>
+                                <span id="status-project" className={`status basis-1/2 max-w-fit ${titleColor} py-1.5 px-3 rounded-full ${btnColor} text-xs`}>
+                                    {project.status}
+                                </span>
+                            </div>
+                            <p className="text-secondary md:text-base mt-1 lg:mt-3 lg:w-96 line-clamp-1">{project.desc}</p>
+                        </div>
+
+                        <div className="web-preview__wrp relative w-full h-fit mt-8">
+                            <img src={project.preview} className="rounded-3xl" alt={`${project.name} preview`} />
+                        </div>
+                        <div className={`overlay__layer w-full h-32 absolute bottom-0 left-1/2 -translate-x-1/2 bg-linear-0 ${overlayColor} from-20%`}></div>
+
+                        {/* Icons Teknologi */}
+                        <TechIcons projectName={project.name} bg={bgColor} />
+                    </div>
+                );
+            })}
+        </>
+    );
+};
+
+export default ProjectCards;
--- src/components/TechIcon.jsx
+++ src/components/TechIcon.jsx
@@ -39,10 +39,10 @@ const TechIcon = ({projectName, bg}) => {
     if (!project) return null;
 
     return (
-        <span id="technologies" onClick={handleClick} className={`group overflow-visible max-w-fit p-2 md:p-2.5 pb-0.5 md:pb-1 flex items-center rounded-xl ${bg} border border-secondary/10 hover:border-secondary/20 backdrop-blur-xs hover:shadow-lg hover:shadow-black/2 active:shadow-lg active:shadow-black/2 absolute bottom-1 md:bottom-4 delay-0 ${isActive ? "bottom-4" : ""} right-1/2 translate-x-1/2 transition-all duration-300 overflow-hidden z-20 scale-90 sm:scale-100`} >
+        <span id="technologies" onClick={handleClick} className={`group overflow-visible max-w-fit p-2 md:p-2.5 pb-0.5 md:pb-1 flex items-center rounded-xl ${bg} border border-secondary/10 hover:border-secondary/20 backdrop-blur-xs hover:shadow-lg hover:shadow-black/2 active:shadow-lg active:shadow-black/2 absolute bottom-7 left-1/2 -translate-x-1/2 transition-all duration-300 overflow-hidden z-20 scale-120 sm:scale-100`} >
 
         {/* Ikon HTML5 (selalu terlihat) */}
-        <span className="logo__wrp not-odd:inline-block text-orange-500">
+        <span className={`logo__wrp not-odd:inline-block transation-all duration-300 delay-75 ${!isActive ? "text-zinc-400" : "text-orange-500"} `}>
             <Icon icon="simple-icons:html5" width="26" height="26" />
             {project.techs.map((item, index) => (
                 <span key={index} className={`count-badge absolute top-[-.5rem] right-[-.5rem] hidden last:grid place-content-center z-20 text-white text-xs scale-80 p-2 rounded-full bg-blue-400 max-w-fit max-h-fit aspect-square animate-bounce font-medium delay-200 md:delay-0 ${isActive ? "opacity-0" : ""} transition-all`}>{index + 1}</span>
@@ -51,7 +51,7 @@ const TechIcon = ({projectName, bg}) => {
 
          {/* Ikon lainnya (muncul saat hover) */}
             {project.techs.map((item, index) => (
-                <span key={index} className={`logo__wrp opacity-0 whitespace-nowrap overflow-hidden max-w-0 transition-all duration-300 delay-300 md:delay-0  ${isActive ? "opacity-100 max-w-[28px] ms-4" : ""}`} >
+                <span key={index} className={`logo__wrp opacity-0 whitespace-nowrap overflow-hidden max-w-0 transition-all duration-300 delay-250 md:delay-0  ${isActive ? "opacity-100 max-w-[28px] ms-3" : ""}`} >
                     <Icon icon={item.icon} width="28" height="28" className={`inline-block ${item.color}`} />
                 </span>
             ))}
